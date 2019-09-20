fa28b3a404d6487ecc9515e30faf1f020e7d1f2e
Alex <26028617+arakek@users.noreply.github.com>
2019-09-20 14:07:14 +0300

    Update notes.js

--- notes.js
+++ notes.js
@@ -6,4 +6,31 @@ function countBs(str, letter) {
 	}
 	return count;
 }
-console.log(countBs("kakkerlak", "k"));
+//console.log(countBs("kakkerlak", "k"));
+//end
+
+/*Сумма диапазона*/
+range = (start, end, h) => {
+	if (h == null)
+		(start > end) ? h = -1 : h = 1
+	let vector = [];
+	if (h > 0) {
+		for (let i = start; i <= end; i += h)
+			vector.push(i);
+	} else {
+		for (let i = start; i >= end; i += h)
+			vector.push(i);
+	}
+	return vector;
+};
+
+sum = (range) => {
+	let s = 0;
+	for (let i of range)
+		s += i;
+	return s;
+};
+
+//console.log(sum(range(5, 2, -1)));
+//console.log(sum(range(5,2)));
+//end
